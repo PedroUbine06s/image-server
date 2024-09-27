b8194e4583ce20835c4f2e9311a3c7fd91a05a3b
Pedro <pedroubine@proton.me>
2024-09-27 16:03:30 -0300

    fix: docker coumpouse production for back-end

--- dockerfile
+++ dockerfile
@@ -3,11 +3,9 @@ FROM node:14
 WORKDIR /usr/src/app
 
 COPY package*.json ./
-
 RUN npm install
 
 COPY . .
+RUN npm run build
 
-EXPOSE 3000
-
-CMD ["npm", "run", "dev"]
+CMD ["npm", "start"]
--- src/config/database.ts
+++ src/config/database.ts
@@ -1,16 +1,17 @@
 
 
 import {Pool} from "pg";
-// const pool = new Pool({
-//     connectionString: process.env.DATABASE_URL,
-// });
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'postgres',
-    password: 'password',
-    port: 5433,
+    connectionString: process.env.DATABASE_URL,
 });
+
+// const pool = new Pool({
+//     user: 'postgres',
+//     host: 'localhost',
+//     database: 'postgres',
+//     password: 'password',
+//     port: 5433,
+// });
 
 export default pool;
